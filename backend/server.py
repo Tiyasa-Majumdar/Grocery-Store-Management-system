1f91b83f5866d0375c3d7c8da1cd382558d8923a
Tiyasa-Majumdar <136687387+Tiyasa-Majumdar@users.noreply.github.com>
2026-09-18 22:38:39 +0530

    Implement error handling in get_products endpoint
    Added error handling for get_products route.

--- backend/server.py
+++ backend/server.py
@@ -28,10 +28,14 @@ connection = get_sql_connection()
 
 @app.route('/getProducts', methods=['GET'])
 def get_products():
-    products = products_dao.get_all_products(connection)
-    response = jsonify(products)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    try:
+        products = products_dao.get_all_products(connection)
+        return jsonify(products)
+    except Exception as e:
+        print("GET PRODUCTS ERROR:", repr(e))
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 @app.route('/getUOM', methods=['GET'])
 def get_uom():
